9c5e3dba009e7772e2fd9cc0fe3cbf123d969d42
ABritex <116909172+ABritex@users.noreply.github.com>
2022-11-07 12:34:23 +0800

    Update and rename BEGORNIA_LAB_ACT4.java to Basic_Login1.java

--- Basic_Login1.java
+++ Basic_Login1.java
@@ -1,25 +1,30 @@
 import java.util.*;
 
-public class BEGORNIA_LAB_ACT4 {
+public class Basic_Login1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String Emails = "Begornia@gmail.com";
-        String Passwords = "Edriel123";
+        String Emails = "123";
+        String Passwords = "123";
         int MidtermGrade, FinaltermGRade, FinalRating;
+        
         System.out.print("Enter your Email    : ");
         String type1 = input.next();
+        
         System.out.print("Enter your Password : ");
         String type2 = input.next();
 
         if (type1.equals(Emails) && type2.equals(Passwords)) {
             System.out.println("Welcome!");
+            
             System.out.print("\nEnter your Midterm grade   : ");
             MidtermGrade = input.nextInt();
+            
             System.out.print("Enter your Finalterm grade : ");
             FinaltermGRade = input.nextInt();
+            
             FinalRating = (40 * MidtermGrade + 60 * FinaltermGRade) / (40 + 60);
-            // (40×89+60×78) / (40+60) = 8240 / 100 = 82.40
+      
             System.out.println("Your final rating is       : " + FinalRating);
             System.out.println("***************************************");
             if (FinalRating > 100) {
