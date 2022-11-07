a206c040704aacfdc36548f41ab7f1e99559bb99
ABritex <116909172+ABritex@users.noreply.github.com>
2022-11-07 08:11:47 +0800

    Add files via upload

---
+++ ABELEDA_LAB_ACT4.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class ABELEDA_LAB_ACT4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String emails[] = { "abeleda48@gmail.com", "Nicholas1@gmail.com" };
+        String passwords[] = { "123456789", "abcdefghi" };
+
+        System.out.print("Enter your Email    : ");
+        String email = s.next();
+        System.out.print("Enter your Password : ");
+        String passowrd = s.next();
+
+        boolean Existing = false;
+        for (int a = 0; a < emails.length; a++) {
+            if (email.equals(emails[a]) && passowrd.equals(passwords[a])) {
+                Existing = true;
+            }
+        }
+        if (Existing) {
+            System.out.println("Welcome!\n");
+            System.out.println("Enter your midterm grade   : ");
+            int Mgrade = s.nextInt();
+            System.out.println("Enter your finalterm grade : ");
+            int Fgrade = s.nextInt();
+            int Frating = (Mgrade + Fgrade) / 2;
+            System.out.println("Your final rating is       : " + Frating);
+            System.out.println("---------------------------------------");
+            if (Frating > 100)
+                System.out.println("Remarks: Invalid Score");
+            else if (Frating >= 75)
+                System.out.println("Remarks: Passed");
+            else if (Frating >= 65)
+                System.out.println("Remarks: Failed");
+            else
+                System.out.println("Remarks: Invalid Score");
+
+        } else
+            System.out.println("Account not found");
+    }
+}
