9b5e57b026e4f783adb9a8162f037d07a11c786e
ABritex <116909172+ABritex@users.noreply.github.com>
2022-11-07 12:53:12 +0800

    Update Basic_Login.java

--- Basic_Login.java
+++ Basic_Login.java
@@ -4,39 +4,54 @@ public class Basic_Login {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        String emails[] = { "abeleda100@gmail.com", "Nics1123@gmail.com" };
+        String emails[] = { "abeleda100@gmail.com", "Nicholas1@gmail.com" };
         String passwords[] = { "123456789", "abcdefghi" };
         int Weight1 = 40, Weight2 = 60;
-
+        String Remarks;
         System.out.print("Enter your Email    : ");
         String email = s.next();
         System.out.print("Enter your Password : ");
         String passowrd = s.next();
 
-        boolean Existing = false;
+        boolean Exist = false;
+
         for (int a = 0; a < emails.length; a++) {
             if (email.equals(emails[a]) && passowrd.equals(passwords[a])) {
-                Existing = true;
+                Exist = true;
             }
         }
-        if (Existing) {
+        if (Exist) {
             System.out.println("Welcome!\n");
-            System.out.println("Enter your midterm grade   : ");
+            System.out.print("Enter your midterm grade   : ");
             int Mgrade = s.nextInt();
-            System.out.println("Enter your finalterm grade : ");
+            System.out.print("Enter your finalterm grade : ");
             int Fgrade = s.nextInt();
             int Frating = (Weight1 * Mgrade + Weight2 * Fgrade) / (Weight1 + Weight2);
+            if (Frating > 100)
+                Remarks = "Invalid";
+            else if (Frating >= 75)
+                Remarks = "Passed";
+            else if (Frating >= 65)
+                Remarks = "Failed";
+            else if (Frating <= 64)
+                Remarks = "Invalid";
+            else
+                Remarks = "Error";
+
             System.out.println("Your final rating is       : " + Frating);
             System.out.println("---------------------------------------");
-            if (Frating > 100)
-                System.out.println("Remarks: Invalid Score");
-            else if (Frating >= 75)
-                System.out.println("Remarks: Passed");
-            else if (Frating >= 65)
-                System.out.println("Remarks: Failed");
-            else
-                System.out.println("Remarks: Invalid Score");
+            System.out.println("Remarks : " + Remarks);
 
+            /*
+             * if (Frating > 100)
+             * System.out.println("Remarks: Invalid Score");
+             * else if (Frating >= 75)
+             * System.out.println("Remarks: Passed");
+             * else if (Frating >= 65)
+             * System.out.println("Remarks: Failed");
+             * else
+             * System.out.println("Remarks: Invalid Score");
+             */
         } else
             System.out.println("Account not found");
     }
